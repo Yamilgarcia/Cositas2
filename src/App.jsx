55008876174c06543b1e7fbc8d3855f9c142b2d5
Yamil <yamilg620@gmail.com>
2025-03-13 09:03:05 -0600

    Cositas 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./database/authcontext";
+import ProtectedRoute from "./components/ProtectedRoute"; 
+import Login from './views/Login'
+import Encabezado from "./components/Encabezado";
+import Inicio from "./views/Inicio";
+
+import "./App.css";
 
 function App() {
-  return(
-   <>
-   <h1>Componente principal</h1>
-   </>
-  )
 
-  
-  
+  return (
+    <>
+      <AuthProvider>
+        <Router>
+          <div className="App">
+            <Encabezado />
+            <main>
+              <Routes>
+                
+                <Route path="/" element={<Login />} />
+                <Route path="/inicio" element={<ProtectedRoute element={<Inicio />} />} />
+
+              </Routes>
+            </main>
+          </div>
+        </Router>
+      </AuthProvider>
+    </>
+  )
 }
 
 export default App
