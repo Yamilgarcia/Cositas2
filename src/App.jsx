4c37bc8d183b01ef24fbc1059506cbdfdd488208
Yamil <yamilg620@gmail.com>
2025-03-17 09:57:28 -0600

    categoria

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./database/authcontext";
 import ProtectedRoute from "./components/ProtectedRoute"; 
 import Login from './views/Login'
 import Encabezado from "./components/Encabezado";
 import Inicio from "./views/Inicio";
+import Categorias from "./views/Categorias"; //Importación de Categorias
 
-import "./App.css";
+import './App.css'
 
 function App() {
 
@@ -21,6 +21,7 @@ function App() {
                 
                 <Route path="/" element={<Login />} />
                 <Route path="/inicio" element={<ProtectedRoute element={<Inicio />} />} />
+                <Route path="/categorias" element={<ProtectedRoute element={<Categorias />} />}/> //Ruta de Categorias protegida
 
               </Routes>
             </main>
